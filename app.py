a3e3a038178ea04a5a05dbbaf01e929a7303a2b1
Zachary R. McKee <zmckee@hawk.iit.edu>
2017-11-24 18:23:22 -0600

    Begun implementing basic login and registration functionality. Added email validation with regular expressions. Added basic home airport validation (predefined hard-coded list, to  be replaced with an SQL query). Added login validation (check a list of users to match an input email address with one in the list). Basic implementation of insertData (Accepts a string flag as the first argument, and then the data to be stored is passed in as a list of arbitrary length of arguments). Registration is done by performing a regex check, which accepts if the user enters a string beginning with upper or lower case R. The regex for an email address is (ANY NON-EMPTY STRING OF CHARACTERS) @ (ANY NON-EMPTY STRING OF CHARACTERS) . (ANY ASCII STRING OF CHARACTERS).

--- app.py
+++ app.py
@@ -25,11 +25,26 @@
 #	Note from Brett: We may have to create a separate 
 #	function that allows the user to create an account.
 
-def customerLogin():
-	userName = input("Enter Username:")
-	password = input("Enter Password:")
 
-	return 1;
+import re
+
+
+# Airports should be populated by an SQL statement along the lines of SELECT id FROM airport.
+# This is plausible data for the use in testing.;
+airports = ["ORD","MDW","MLI","PIA","DTW","JFK","SEA"]
+users = []
+def customerLogin():
+
+	newUser = input("Enter R[egister] for registration, anything else for login: ")
+	regex = re.compile('R.*',re.IGNORECASE) # Matches any string input beginning with R
+	if(regex.match(newUser)): # This will initiate if the user requests registration (by typing in R followed by any other text
+		customerRegister()
+	print("^+^+^ User Login ^+^+^")
+	userName = input("Enter Username: ")
+	if userName in [i[0] for i in users]:
+		print("Login Successful -- welcome!")
+	return;
+
 
 
 #	2)
@@ -39,5 +54,41 @@ def customerLogin():
 #	insert credit card information, addresses, book
 #	flights, etc.
 
-def insertData():
+def insertData(typeOfInsertion,*args):
+	if(typeOfInsertion == "REG"):
+		# insert into database a new customer
+		users.append((args[0],args[1],args[2],args[3],args[4]))
+		
 	return 1;
+
+
+	
+def customerRegister():
+	print("-=-=- New Customer Registration -=-=-")
+	print("You will be prompted for an email address, first name, middle initial,\n" +
+	      "last name, and home airport (identified by its 3-character ID code).\n"	+  
+	      "You can add payment information/addresses on initial login.")
+	
+	validEmails  = re.compile('^(.+)@(.+)\.(\w+)$',re.IGNORECASE) 	# Regex expression used to 
+									# validate email address input
+	email 	= input("Enter email: ")
+	while(validEmails.match(email) == None): # If the email is invalid
+		print("'" + email + "'" + " is not a valid email.")
+		email = input("Enter a valid email: ")	
+	
+	first 	= input("Enter first name: ")
+
+	middle 	= input("Enter middle initial: ")
+
+	last 	= input("Enter last name: ")
+	
+	# An airport must exist before a customer can call it their home
+	
+	home 	= input("Enter home airport: ")
+	while(home not in airports):
+		print("'" + home + "'" + "does not exist.")
+		home = input("Enter a valid home airport: ")
+	insertData("REG",email,first,middle,last,home)
+	print("Registration complete, you will now be brought to the login prompt.")
+if __name__ == "__main__":
+	customerLogin()
