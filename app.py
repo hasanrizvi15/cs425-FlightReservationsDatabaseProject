bf02f2eafd35b0b61f27be459daac94200a7b828
Hasan Rizvi <srizvi13@hawk.iit.edu>
2017-11-29 09:15:13 +0000

    Added customer menu + handler functions for each option

--- app.py
+++ app.py
@@ -87,9 +87,42 @@ def customerLogin():
 			customerMenu(user)
 
 def customerMenu(user):
-	print("#@#@#@# " + user[1] + "'s" + " User Menu #@#@#@")
-	return;
+	
+	switch = {"1": addressHandler, 
+			  "2": paymentHandler, 
+			  "3": searchFlights, 
+			  "4": bookFlight, 
+			  "q": initialPrompt}
 
+	print("\033c")
+	print("#@#@#@# " + user[1] + "'s" + " User Menu #@#@#@")
+
+	menu = """  Please select an option from the list:
+
+	 	 __________________________________________________
+	 	| OPTION   | DESCRIPTION                           |
+	 	|__________|_______________________________________|
+	 	|   (1)    | Manage Addresses                      |
+	 	|__________|_______________________________________|
+	 	|   (2)    | Manage Payment Methods                |
+	 	|__________|_______________________________________|
+	 	|   (3)    | Search Flights                        |
+	 	|__________|_______________________________________|
+	 	|   (4)    | Book Flight                           |
+	 	|__________|_______________________________________|
+	 	|   (q)    | QUIT                                  |
+	 	|__________|_______________________________________| \n"""
+
+	while (1):
+		print(menu)
+		option = input("\n [?]: ")
+
+		if option in switch:
+				switch[option](user)
+		else:
+			print("Invalid option \n")
+
+		print("\033c")
 
 
 
@@ -113,11 +146,44 @@ def insertData(typeOfInsertion,*args): # Return False on insertion fail, True ot
 			print("Could not insert new user into database")
 			print(error)
 			conn.commit()
-			return False;
+			return False
 		conn.commit()
 		#users.append((args[0],args[1],args[2],args[3],args[4]))
-		return True;
-	return;
+		return True
+
+	elif(typeOfInsertion == "ADDR"):
+	    cmd = "INSERT INTO address (street, city, state, country, zipcode, user_) VALUES (%s,%s,%s,%s,%d,%s)"
+	    for i in args:
+	    	print(i)
+
+	    try:
+	    	cursor.execute(cmd,(args[0],args[1],args[2],args[3],args[4],args[5]))
+	    except Exception as error:
+	    	print("Could not insert new address into database")
+	    	print(error)
+	    	conn.commit()
+	    	return False
+
+	    conn.commit()
+	    return True
+
+	elif(typeOfInsertion == "PAYM"):
+	    return True 
+	
+
+def removeData(typeOfDeletion, pkey):
+	if(typeOfDeletion == "ADDR"):
+		cmd = "DELETE FROM address WHERE address_id={}".format(pkey)
+		try: 
+			cursor.execute(cmd)
+		except Exception as error:
+			print("Could not remove address")
+			print(error)
+			conn.commit()
+			return False
+		conn.commit()
+		return True 
+	return
 	
 def customerRegister():
 	print("-=-=- New Customer Registration -=-=-")
@@ -149,5 +215,96 @@ def customerRegister():
 		registered = insertData("REG",email,first,middle,last,home)
 	print("Registration complete, you will now be brought to the login prompt.")
 	customerLogin()
+
+# Add/Modify Address
+def addressHandler(user):
+	success = False
+	print("\033c")
+	print("""  Please select an option from the list:
+	 	 __________________________________________________
+	 	| OPTION   | DESCRIPTION                           |
+	 	|__________|_______________________________________|
+	 	| A[dd]    | Add Address                           |
+	 	|__________|_______________________________________|
+	 	| R[emove] | Delete Address                        |
+	 	|__________|_______________________________________|\n""")
+
+	option = input()
+	print("\033c")
+
+	if (option.lower() == "a"):
+		street = input("Enter street: ")
+		city = input("Enter city: ")
+		state = input("Enter state: ")
+		country = input("Enter country: ")
+		zipcode = input("Enter zipcode: ") 
+		
+		success = insertData("ADDR", street, city, state, country, zipcode, user[0])  
+	
+	elif (option.lower() == "r"):
+		try:
+			cursor.execute("SELECT * FROM address WHERE user_={}".format(user[0]))
+			addresses = cursor.fetchall()
+			print("Displaying current addresses, please select the address to be removed")
+			i = 0
+			for addr in addresses: 
+				print("[{}]: {}, {}, {}, {}. {}".format(i, addr[1], addr[2], addr[3], addr[4], addr[5]))
+				i += 1
+			idx = input("[?]: ")
+			if(idx <= i and idx >=0):
+				success = removeData("ADDR", addresses[i][0])
+
+		except Exception as error:
+			print(error)
+	else:
+		print("Invalid choice, returning to main menu.")
+	
+	if (success): 
+		print("Update successful")
+	else:
+		print("Update failed") 
+	
+	customerMenu(user)
+
+# Add/Modify Payment Methods
+def paymentHandler(user):
+	print("\033c")
+	print("""  Please select an option from the list:
+	 	 __________________________________________________
+	 	| OPTION   | DESCRIPTION                           |
+	 	|__________|_______________________________________|
+	 	| A[dd]    | Add Credit card                       |
+	 	|__________|_______________________________________|
+	 	| R[emove] | Delete credit card                    |
+	 	|__________|_______________________________________|\n""")
+
+	option = input()
+	if (option.lower() == "a"):
+		# TODO
+		insertData("ADDR", )   
+	elif (option.lower() == "r"):
+		# TODO
+		removeData("ADDR", )   
+	else:
+		print("Invalid choice, returning to main menu.")
+
+	if (success): 
+		print("Update successful")
+	else:
+		print("Update failed")
+
+	customerMenu(user)
+
+# Handle flight search
+def searchFlights(user):
+	return
+
+# Handle booking
+def bookFlight(user):
+	return
+
+
 if __name__ == "__main__":
 	initialPrompt()
+
+
